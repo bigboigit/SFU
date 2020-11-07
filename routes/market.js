7a23296d137303d5f047328f904869742a41ee8a
Anthony Ting <anthonydting@gmail.com>
2020-11-07 15:44:11 -0800

    fix some friends and owners

--- routes/market.js
+++ routes/market.js
@@ -4,49 +4,60 @@ var router = express.Router();
 const knex = require('../databases')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
   // owner table
-  knex('market')
-    .join('market.friendship', '=', 'inventory.id')
-    .join('inventory.owner', '=', 'users.id')
-    .select('market.id', 'users.username','market.price')
-  
+  const owners = await knex('market')
+    .join('inventory', {
+      'market.friendship': 'inventory.id'
+    })
+    .join('users', {
+      'inventory.owner': 'users.id'
+    })
+    .select('market.id', 'users.username', 'market.price')
+    .orderBy('id', 'asc');
+
   // friend table
-  knex('market')
-    .join('market.friendship', '=', 'inventory.id')
-    .join('inventory.friend', '=', 'users.id')
+  const friends = await knex('market')
+    .join('inventory', {
+      'market.friendship': 'inventory.id'
+    })
+    .join('users', {
+      'inventory.friend': 'users.id'
+    })
     .select('market.id',
-            'users.username', 
-            'users.gpa', 
-            'users.program', 
-            'users.hobbies', 
-            'users.personailty', 
-            'users.city', 
-            'users.country', 
-            'market.price')
+      'users.username',
+      'users.gpa',
+      'users.program',
+      'users.hobbies',
+      'users.personality',
+      'users.city',
+      'users.country',
+      'market.price')
+    .orderBy('id', 'asc');
 
   res.render('market', {
-    title: 'Log in'
+    owners: owners,
+    friends: friends
   });
 });
 
 // take off market place
-router.post('/buy', function(req, res, next) {
+router.post('/buy', function (req, res, next) {
   /* UPDATE transaction server with buy
      DROP previous transaction of previous owner 
      update credit of buyer and seller
      */
-    knex ('market').where('id', req.query.id).del()
+  knex('market').where('id', req.query.id).del()
 
-    knex ('inventory').insert({
-      owner: req.query.purchaser,
-      friend: req.query.friend
-    })
+  knex('inventory').insert({
+    owner: req.query.purchaser,
+    friend: req.query.friend
+  })
 });
 
 // put on marketplace
-router.post('/sell', function(req, res, next) {
-  knex ('market').insert({
+router.post('/sell', function (req, res, next) {
+  knex('market').insert({
     friendship: req.query.friend,
     price: req.query.price
   })
