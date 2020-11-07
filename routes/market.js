3c82f0bcb8fc7da9ee6e99d19fd3bdc924dd7997
tnc1 <tnc1@sfu.ca>
2020-11-07 14:23:11 -0800

    buy and sell logic should be good?

--- routes/market.js
+++ routes/market.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const knex = require('../databases')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('market', {
@@ -8,16 +10,26 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// take off market place
 router.post('/buy', function(req, res, next) {
   /* UPDATE transaction server with buy
      DROP previous transaction of previous owner 
      update credit of buyer and seller
      */
+    knex ('market').where('id', req.query.id).del()
+
+    knex ('inventory').insert({
+      owner: req.query.purchaser,
+      friend: req.query.friend
+    })
 });
 
+// put on marketplace
 router.post('/sell', function(req, res, next) {
-  /* Place friend on marketplace
-     */
+  knex ('market').insert({
+    friendship: req.query.friend,
+    price: req.query.price
+  })
 });
 
 module.exports = router;
