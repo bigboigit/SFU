ec2bdab582880f463c16510080b33fda5a72e870
tnc1 <tnc1@sfu.ca>
2020-11-07 18:37:45 -0800

    value calculations are working

--- routes/market.js
+++ routes/market.js
@@ -5,7 +5,7 @@ const knex = require('../databases')
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
-  // owner table
+  // owner table in market
   const owners = await knex('market')
     .join('inventory', {
       'market.friendship': 'inventory.id'
@@ -16,7 +16,7 @@ router.get('/', async function (req, res, next) {
     .select('market.id', 'users.username', 'market.price', 'users.id as userId')
     .orderBy('market.id', 'asc');
 
-  // friend table
+  // friend table in market
   const friends = await knex('market')
     .join('inventory', {
       'market.friendship': 'inventory.id'
@@ -36,6 +36,70 @@ router.get('/', async function (req, res, next) {
       'market.price')
     .orderBy('market.id', 'asc');
 
+  let friendValues = [];
+  for (i = 0; i < friends.length; i ++) {
+    var value = 0;
+    // gpa value
+    // difference of gpa's * 3
+    value -= Math.abs((req.user.pref_gpa - friends[i].gpa) * 3)
+    // program value
+    if (friends[i].program == 1) {
+      value += req.user.fas;
+    }
+    else if (friends[i].program == 2) {
+      value += req.user.arts;
+    }
+    else if (friends[i].program == 3) {
+      value += req.user.bus;
+    }
+    else if (friends[i].program == 4) {
+      value += req.user.comm;
+    }
+    else if (friends[i].program == 5) {
+      value += req.user.educ;
+    }
+    else if (friends[i].program == 6) {
+      value += req.user.env;
+    }
+    else if (friends[i].program == 7) {
+      value += req.user.hsci;
+    }
+    else if (friends[i].program == 8) {
+      value += req.user.sci;
+    }
+    // matching hobbies
+    if (friends[i].hobbies.includes(req.user.hobbies[0])) {
+      value += 2;
+    }
+    if (friends[i].hobbies.includes(req.user.hobbies[1])) {
+      value += 2;
+    }
+    if (friends[i].hobbies.includes(req.user.hobbies[2])) {
+      value += 2;
+    }
+    // personality value
+    switch (friends[i].personality) {
+      case 1:
+        value += req.user.agreeable;
+        break;
+      case 2:
+        value += req.user.conscientious;
+        break;
+      case 3:
+        value += req.user.extravert;
+        break;
+      case 4:
+        value += req.user.neurotic;
+        break;
+      case 5:
+        value += req.user.open;
+        break;
+    }
+    friendValues.push(value);
+  }
+  console.log();
+  console.log(friendValues);
+  console.log();
   res.render('market', {
     owners: owners,
     friends: friends
