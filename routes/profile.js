e82cfd56c145922dcee2a3469baabc6963e72170
Anthony Ting <anthonydting@gmail.com>
2020-11-07 18:53:29 -0800

    fix profile problems

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const knex = require('../databases');
 
 /* GET users listing. */
-router.get('/:pid', async function(req, res, next) {
+router.get('/:pid', async function (req, res, next) {
   console.log(req.params.pid)
   const profileowner = await knex('users')
     .select('*')
@@ -14,25 +14,32 @@ router.get('/:pid', async function(req, res, next) {
     .catch(err => {
       return null
     })
-  
+
   res.render('profile', {
     title: req.params.pid,
-    me: (profileowner)? ((profileowner.length==0)? null: profileowner[0]) : null
+    me: (profileowner) ? ((profileowner.length == 0) ? null : profileowner[0]) : null
   });
 });
 
 /* GET user's own profile */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   const friends = await knex('inventory')
     .join('users', {
       'inventory.friend': 'users.id'
     })
     .select('*')
     .where('inventory.owner', req.user.id);
-    
+
+  let tiers = [];
+  for (let i = 0; i < friends.length; i++) {
+    tiers.push(Math.floor(Math.random() * friends.length));
+  }
+
   res.render('profile', {
     title: req.user.username,
-    me: req.user
+    me: req.user,
+    friends: friends,
+    values: tiers
   });
 });
 
